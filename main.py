a50dc23f280034e55a2a330cd99649b68bbc77d1
GR_Team_Boss <rabbitwow97@gmail.com>
2021-07-10 21:34:30 +0500

    add many text

--- main.py
+++ main.py
@@ -380,26 +380,241 @@ from django.shortcuts import render
         APP_LINKS = ['admin.py', 'apps.py', 'models.py', 'tests.py', 'views.py']
         for i in PROJECT_LINKS:
             with open(f'./{parent_folder}/{project}/{i}', 'w+', encoding='utf-8') as file:
-                read = file.read().split('\n')
                 if i == 'asgi.py':
-                    pass
+                    file.write(
+f"""
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{project}.settings')
+
+application = get_asgi_application()
+""")
                 if i == 'settings.py':
-                    pass
+                    read = file.read().split('\n')
+                    log = []
+                    for i in read:
+                        log.append(i.find('SECRET_KEY'))
+                    for i in log:
+                        if i == 0:
+                            key = read[log.index(i)]
+                    file.write(
+"""
+
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+%s
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    %s
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = '%s.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR , 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = '%s.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATIC_URL = '/static/'                    
+""" % (key, ',\n'.join(f'{x}.apps.{x}Config,' for x in app), project, project))
                 if i == 'urls.py':
-                    pass
+                    file.write(
+f"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("{app}/", include('{app}.urls'))
+]
+""")
                 if i == 'wsgi.py':
-                    pass
+                    file.write(
+f"""
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '{project}.settings')
+
+application = get_wsgi_application()
+""")
         for i in app:
             for n in APP_LINKS:
                 with open(f'./{parent_folder}/{i}/{n}', 'w+', encoding='utf-8') as file:
-                    read = file.read().split('\n')
                     if n == 'admin.py':
-                        pass
+                        file.write(
+"""
+from django.contrib import admin
+
+from .models import Model1, Model2
+
+# Register your models here.
+
+class Default(args):
+    model = Model2
+    extra = 3
+
+
+class Default2(args):
+    fieldsets = [
+        (None,               {'fields': ['text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [Default]
+    list_display = ('text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['text']
+
+admin.site.register(Model1, Defautlt2)
+admin.site.register(Model2)
+""")
                     if n == 'apps.py':
-                        pass
+                        file.write(
+f"""
+from django.apps import AppConfig
+
+
+class {i}Config(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = '{i}'
+""")
                     if n == 'models.py':
-                        pass
+                        file.write(
+"""
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+
+# Create your models here.
+class Model1(models.Model):
+    text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    
+    
+    def __str__(self):
+        return self.text
+    
+
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    
+    
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+
+class Model2(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    
+    def __str__(self):
+        return self.choice_text
+""")
                     if n == 'tests.py':
-                        pass
+                        file.write()
                     if n == 'views.py':
-                        pass
+                        file.write()
