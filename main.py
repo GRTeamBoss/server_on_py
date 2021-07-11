ba6d6d3d07fcf7ec9ef59ed0cf66691cfe1b7c17
GR_Team_Boss <rabbitwow97@gmail.com>
2021-07-11 17:00:33 +0500

    release=v0.1.0

--- main.py
+++ main.py
@@ -320,6 +320,7 @@ from django.shortcuts import render
             read = '\n'.join(read)
             file.write(read)
         return 0
+        
     
     def install(method, arg):
         """
@@ -347,11 +348,11 @@ from django.shortcuts import render
         Server.system(f'cd {parent_folder}; python manage.py migrate')
 
 
-    def run(port = 8000):
-        Server.system(f'python manage.py runserver {port}')
+    def run(parent_folder, port = 8000):
+        Server.system(f'cd {parent_folder}; python manage.py runserver {port}')
     
 
-    def create(method, name = 'default', content = ''):
+    def create(method, name, content = ''):
         """
         1 method:
         create folder:
@@ -377,7 +378,7 @@ from django.shortcuts import render
 
     def edit(parent_folder = 'default', project = 'default', app = ['widget',]):
         PROJECT_LINKS = ['asgi.py', 'settings.py', 'urls.py', 'wsgi.py']
-        APP_LINKS = ['admin.py', 'apps.py', 'models.py', 'tests.py', 'views.py']
+        APP_LINKS = ['admin.py', 'apps.py', 'models.py', 'tests.py', 'views.py', 'urls.py']
         for i in PROJECT_LINKS:
             with open(f'./{parent_folder}/{project}/{i}', 'w+', encoding='utf-8') as file:
                 if i == 'asgi.py':
@@ -615,6 +616,209 @@ class Model2(models.Model):
         return self.choice_text
 """)
                     if n == 'tests.py':
-                        file.write()
+                        file.write(
+f"""
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Model2
+# Create your tests here.
+
+class QuestionDetailViewTests(TestCase):
+    def test_future_question(self):
+        "
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        "
+        future_question = create_question(text='Future question.', days=5)
+        url = reverse('{i}:detail', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        "
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        "
+        past_question = create_question(text='Past Question.', days=-5)
+        url = reverse('{i}:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.text)
+
+
+def create_question(text, days):
+    "
+    Create a question with the given `text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    "
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(text=text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        "
+        If no questions exist, an appropriate message is displayed.
+        "
+        response = self.client.get(reverse('{i}:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_past_question(self):
+        "
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        "
+        question = create_question(text="Past question.", days=-30)
+        response = self.client.get(reverse('{i}:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_future_question(self):
+        "
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        "
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('{i}:index'))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+
+    def test_future_question_and_past_question(self):
+        "
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        "
+        question = create_question(text="Past question.", days=-30)
+        create_question(text="Future question.", days=30)
+        response = self.client.get(reverse('{i}:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        "
+        The questions index page may display multiple questions.
+        "
+        question1 = create_question(text="Past question 1.", days=-30)
+        question2 = create_question(text="Past question 2.", days=-5)
+        response = self.client.get(reverse('{i}:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1],
+        )
+
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        "
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        "
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_old_question(self):
+        "
+        was_published_recently() returns False for questions whose pub_date
+        is older than 1 day.
+        "
+        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        "
+        was_published_recently() returns True for questions whose pub_date
+        is within the last day.
+        "
+        time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=time)
+        self.assertIs(recent_question.was_published_recently(), True)
+""")
                     if n == 'views.py':
-                        file.write()
+                        file.write(
+"""
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+
+from .models import Model1, Model2
+
+# Create your views here.
+class IndexView(generic.ListView):
+    template_name = '%s/index.html'
+    context_object_name = 'latest_question_list'
+
+
+    def get_queryset(self):
+        "
+        Return the last five published questions (not including those set to be
+        published in the future).
+        "
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = '%s/detail.html'
+    def get_queryset(self):
+        "
+        Excludes any questions that aren't published yet.
+        "
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = '%s/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, '%s/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('%s:results', args=(question.id,)))
+""" % (i, i, i, i, i))
+                    if n == 'urls.py':
+                        file.write(
+f"""
+from django.urls import path
+
+from . import views
+
+app_name = '{i}'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+""")
